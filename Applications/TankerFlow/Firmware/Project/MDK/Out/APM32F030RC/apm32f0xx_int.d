d9a47653f778bbcfbd9e4d255d661ab2b67cb0dc
stephen <1073605179@qq.com>
2026-09-20 17:00:55 +0800

    chore(tankerflow): remove generated build artifacts from repository

--- Applications/TankerFlow/Firmware/Project/MDK/Out/APM32F030RC/apm32f0xx_int.d
+++
@@ -1,6 +0,0 @@
-.\out\apm32f030rc\apm32f0xx_int.o: ..\..\Source\apm32f0xx_int.c
-.\out\apm32f030rc\apm32f0xx_int.o: ..\..\Include\apm32f0xx_int.h
-.\out\apm32f030rc\apm32f0xx_int.o: ..\..\Include\bsp_gpio.h
-.\out\apm32f030rc\apm32f0xx_int.o: D:\soft\codeSoft\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\out\apm32f030rc\apm32f0xx_int.o: ..\..\Include\bsp_tick.h
-.\out\apm32f030rc\apm32f0xx_int.o: ..\..\Include\bsp_uart.h
--- Applications/TankerFlow/Firmware/Project/MDK/Out/APM32F030RC/startup_apm32f030.d
+++
@@ -1,1 +0,0 @@
-.\out\apm32f030rc\startup_apm32f030.o: ..\..\..\..\..\Libraries\Device\Geehy\APM32F0xx\Source\arm\startup_apm32f030.s
